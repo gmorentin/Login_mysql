27aa0c87022875003476bd0d994418f437c1b106
Gustavo Morentin <gmorentin@ucol.mx>
2018-05-30 09:34:15 -0500

    cambio de las bvistas por hbs

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,13 @@
 // app/routes.js
 module.exports = function (app, passport) {
-
+	app.set('view engine', 'hbs');
 	app.get('/', function (req, res) {
-		res.render('login.ejs', { message: req.flash('loginMessage') }); // load the index.ejs file
+		res.render('login.hbs', { message: req.flash('loginMessage') }); // load the index.ejs file
 	});
 
 	app.get('/login', function (req, res) {
 		// render the page and pass in any flash data if it exists
-		res.render('login.ejs', { message: req.flash('loginMessage') });
+		res.render('login.hbs', { message: req.flash('loginMessage') });
 	});
 
 
@@ -26,7 +26,7 @@ module.exports = function (app, passport) {
 
 	app.post('/login', passport.authenticate('local-login', {
 		//successRedirect: '/profile', // redirect to the secure profile section
-		//failureRedirect: '/login', // redirect back to the signup page if there is an error
+		failureRedirect: '/login', // redirect back to the signup page if there is an error
 		failureFlash: true // allow flash messages
 	}),
 		function (req, res) {
@@ -55,7 +55,7 @@ module.exports = function (app, passport) {
 
 	app.get('/signup', function (req, res) {
 		// render the page and pass in any flash data if it exists
-		res.render('signup.ejs', { message: req.flash('signupMessage') });
+		res.render('signup.hbs', { message: req.flash('signupMessage') });
 	});
 
 	// process the signup form
@@ -67,12 +67,15 @@ module.exports = function (app, passport) {
 
 
 	app.get('/profile', isLoggedIn, function (req, res) {
-		res.render('profile.ejs', {
-			user: req.user // get the user out of session and pass to template
-		});
+		if(req.user.tipo==='Gerente'){
+			res.render('profile.hbs', {
+				user: req.user // get the user out of session and pass to template
+			});
+		}
+		else res.redirect('/');
 	});
 
-	
+
 	app.get('/logout', function (req, res) {
 		req.logout();
 		res.redirect('/');
@@ -81,7 +84,6 @@ module.exports = function (app, passport) {
 
 // route middleware to make sure
 function isLoggedIn(req, res, next) {
-
 	// if user is authenticated in the session, carry on
 	if (req.isAuthenticated())
 		return next();
--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 // config/passport.js
 
 // load all the things we need
-var LocalStrategy   = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 
 // load up the user model
 var mysql = require('mysql');
@@ -10,7 +10,7 @@ var dbconfig = require('./database');
 var connection = mysql.createConnection(dbconfig.connection);
 //connection.query('USE Login');
 // expose this function to our app using module.exports
-module.exports = function(passport) {
+module.exports = function (passport) {
 
     // =========================================================================
     // passport session setup ==================================================
@@ -19,13 +19,13 @@ module.exports = function(passport) {
     // passport needs ability to serialize and unserialize users out of session
 
     // used to serialize the user for the session
-    passport.serializeUser(function(user, done) {
+    passport.serializeUser(function (user, done) {
         done(null, user.id);
     });
 
     // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        connection.query("SELECT * FROM users2 WHERE id = ? ",[id], function(err, rows){
+    passport.deserializeUser(function (id, done) {
+        connection.query("SELECT * FROM users2 WHERE id = ? ", [id], function (err, rows) {
             done(err, rows[0]);
         });
     });
@@ -40,39 +40,39 @@ module.exports = function(passport) {
         'local-signup',
         new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
-            usernameField : 'username',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
+            usernameField: 'username',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass back the entire request to the callback
         },
-        function(req,username, password,done) {
-            //console.log(req.body);
+            function (req, username, password, done) {
+                //console.log(req.body);
 
-            // find a user whose email is the same as the forms email
-            // we are checking to see if the user trying to login already exists
-            connection.query("SELECT * FROM users2 WHERE username = ?",[username], function(err, rows) {
-                if (err)
-                    return done(err);
-                if (rows.length) {
-                    return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
-                } else {
-                    // if there is no user with that username
-                    // create the user
-                    var newUserMysql = {
-                        username: username,
-                        password: bcrypt.hashSync(password, null, null),  // use the generateHash function in our user model
-                        tipo: req.body.tipo
-                    };
+                // find a user whose email is the same as the forms email
+                // we are checking to see if the user trying to login already exists
+                connection.query("SELECT * FROM users2 WHERE username = ?", [username], function (err, rows) {
+                    if (err)
+                        return done(err);
+                    if (rows.length) {
+                        return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+                    } else {
+                        // if there is no user with that username
+                        // create the user
+                        var newUserMysql = {
+                            username: username,
+                            password: bcrypt.hashSync(password, null, null),  // use the generateHash function in our user model
+                            tipo: req.body.tipo
+                        };
 
-                    var insertQuery = "INSERT INTO users2 ( username, password, tipo) values (?,?,?)";
+                        var insertQuery = "INSERT INTO users2 ( username, password, tipo) values (?,?,?)";
 
-                    connection.query(insertQuery,[newUserMysql.username, newUserMysql.password, req.body.tipo],function(err, rows) {
-                        newUserMysql.id = rows.insertId;
+                        connection.query(insertQuery, [newUserMysql.username, newUserMysql.password, req.body.tipo], function (err, rows) {
+                            newUserMysql.id = rows.insertId;
 
-                        return done(null, newUserMysql);
-                    });
-                }
-            });
-        })
+                            return done(null, newUserMysql);
+                        });
+                    }
+                });
+            })
     );
 
     // =========================================================================
@@ -85,26 +85,27 @@ module.exports = function(passport) {
         'local-login',
         new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
-            usernameField : 'username',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
+            usernameField: 'username',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass back the entire request to the callback
         },
-        function(req, username, password, done) { // callback with email and password from our form
-            
-            connection.query("SELECT * FROM users2 WHERE username = ?",[username], function(err, rows){  
-                if (err)
+            function (req, username, password, done) { // callback with email and password from our form
+                
+                connection.query("SELECT * FROM users2 WHERE username = ?", [username], function (err, rows) {
+                    if (err)
                     return done(err);
-                if (!rows.length) {
-                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-                }
+                    if (!rows.length) {
+                        //console.log(req.body);
+                        return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                    }
 
-                // if the user is found but the password is wrong
-                if (!bcrypt.compareSync(password, rows[0].password))
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                    // if the user is found but the password is wrong
+                    if (!bcrypt.compareSync(password, rows[0].password))
+                        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
-                // all is well, return successful user
-                return done(null, rows[0]);
-            });
-        })
+                    // all is well, return successful user
+                    return done(null, rows[0]);
+                });
+            })
     );
 };
